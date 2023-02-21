ea610e1ed3864ba1afc8fa492a54386fbecf5016
adrian411997 <adrian.mart41197@gmail.com>
2023-02-20 21:01:10 -0300

    services-changes

--- src/Componentes/main/Servicio/Servicios.jsx
+++ src/Componentes/main/Servicio/Servicios.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Servicios.css";
 
 const Servicios = () => {
@@ -22,7 +23,7 @@ const Servicios = () => {
           </h1>
         </div>
         <div className="card-services-container">
-          <div className="card-service-list">
+          {/*           <div className="card-service-list">
             <div className={`service-card`}>
               <div className="background1 marketing"></div>
               <div className="content-card-service">
@@ -58,6 +59,77 @@ const Servicios = () => {
                   <button>Conoce mas</button>
                 </div>
               </div>
+            </div>
+          </div>*/}
+          <div className="card-service">
+            <div className="bar"></div>
+            <div className="title-card">
+              <h1>Marketing</h1>
+            </div>
+            <div className="content">
+              <p>
+                Posicionamos de manera estrategica tu negocio. A traves de la
+                creacion de contenido en redes sociales, logramos el alcance que
+                necesitas.
+              </p>
+            </div>
+            <div className="button-card">
+              <button>
+                <Link>Conocer mas</Link>
+              </button>
+            </div>
+          </div>
+          <div className="card-service">
+            <div className="bar"></div>
+            <div className="title-card">
+              <h1>Desarrollo Web</h1>
+            </div>
+            <div className="content">
+              <p>
+                Tu marca con dominio propio para estar presente en la red. Una
+                gestión adaptada a tu negocio. Todo lo necesario para que
+                obtengas un sitio web de excelencia
+              </p>
+            </div>
+            <div className="button-card">
+              <button>
+                <Link>Conocer mas</Link>
+              </button>
+            </div>
+          </div>
+          <div className="card-service">
+            <div className="bar"></div>
+            <div className="title-card">
+              <h1>DISEÑO GRÁFICO</h1>
+            </div>
+            <div className="content">
+              <p>
+                Desarrollamos diferentes estrategias para conseguir una
+                comunicación visual adaptada a las necesidades de tu marca.
+              </p>
+            </div>
+            <div className="button-card">
+              <button>
+                <Link>Conocer mas</Link>
+              </button>
+            </div>
+          </div>
+          <div className="card-service">
+            <div className="bar"></div>
+            <div className="title-card">
+              <h1>PUBLICIDAD DIGITAL</h1>
+            </div>
+            <div className="content">
+              <p>
+                Un desarrollo hiperdirecto de llegar a la audiencia que deseas
+                en redes sociales. Logrando resultados reales maximizando tus
+                gastos.
+              </p>
+            </div>
+            <div className="button-card">
+              <button>
+                <Link>Conocer mas</Link>
+              </button>
             </div>
           </div>
         </div>
