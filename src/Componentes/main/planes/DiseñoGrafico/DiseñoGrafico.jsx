283d4e8757b05262436e7240bd856ee810f646b3
adrian411997 <adrian.mart41197@gmail.com>
2023-02-27 21:13:41 -0300

    changes-design-plan

--- src/Componentes/main/planes/DiseñoGrafico/DiseñoGrafico.jsx
+++ src/Componentes/main/planes/DiseñoGrafico/DiseñoGrafico.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../../utilities/Header/Header";
 import Footer from "../../../utilities/Footer/Footer";
 import "./DiseñoGrafico.css";
 
 const DiseñoGrafico = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <Header />
@@ -17,6 +20,83 @@ const DiseñoGrafico = () => {
             empresa la imagen que se merece siguiendo sus lineamientos e
             ideales.
           </p>
+        </div>
+      </div>
+      <div className="design-graphic-intro-content">
+        <div className="design-graphic-intro-container">
+          <div className="design-graphic-into-title">
+            <h1 className="title-black">
+              <div className="barra-black"></div>
+              PORQUE ES IMPORTANTE EL DISEÑO?
+            </h1>
+          </div>
+          <div className="design-graphic-intro-writting">
+            <div className="design-text">
+              <p>
+                La imagen empresarial o corporativa es como se mostrará su
+                empresa ante los demás.
+              </p>
+              <p>
+                Un buen diseño facilitará que el público se identifique con la
+                marca, reconociéndose en los valores, principios y filosofía de
+                la empresa
+              </p>
+            </div>
+            <div className="design-graphic-intro-images">
+              <div className="photos-graphic">
+                <img
+                  src="https://res.cloudinary.com/dni5cjwpu/image/upload/v1677531485/metrica/chocaloteeeee-removebg-preview_xn0xfk.png"
+                  alt=""
+                  width={250}
+                />
+                <img
+                  src="https://res.cloudinary.com/dni5cjwpu/image/upload/v1677531512/metrica/entretre_logo_lineas_v2bnjw.png"
+                  alt=""
+                  width={250}
+                />
+                <img
+                  src="https://res.cloudinary.com/dni5cjwpu/image/upload/v1677531519/metrica/Recurso_3xxxhdpi_dtkltt.png"
+                  alt=""
+                  width={250}
+                />
+                <img
+                  src="https://res.cloudinary.com/dni5cjwpu/image/upload/v1677531526/metrica/PELUQUERIA_CANINA_zpx9oh.jpg"
+                  alt=""
+                  width={250}
+                />
+                <img
+                  src="https://res.cloudinary.com/dni5cjwpu/image/upload/v1677532011/metrica/EXTERIOR_klx4eo.jpg"
+                  alt=""
+                  width={250}
+                />
+                <img
+                  src="https://res.cloudinary.com/dni5cjwpu/image/upload/v1677531536/metrica/1ecologo_wrnf69.png"
+                  alt=""
+                  width={250}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="design-graphic-abarcamos">
+        <div className="design-graphic-abarcamos-container">
+          <div className="design-graphic-abarcamos-title">
+            <h1 className="title-black">
+              <div className="barra-black"></div>EN METRICA ABARCAMOS...
+            </h1>
+          </div>
+          <div className="design-graphic-abarcamos-content">
+            <div className="abarc-list">
+              <li>Diseño de logo</li>
+              <li>Imagen corporativa</li>
+              <li>Diseño publicitario</li>
+              <li>Ilustración e imagenes digitales</li>
+              <li>Diseño de interiores y serialización</li>
+              <li>Editorial</li>
+              <li>Rebranding</li>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
